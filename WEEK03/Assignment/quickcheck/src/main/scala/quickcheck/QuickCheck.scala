3745faf6a50f6b8ef71c1a0b1f33274f6da8ab1b
oonisim <monishi@wynyardgroup.com>
2016-09-02 00:59:28 +1000

    ScalaCheck completed

--- WEEK03/Assignment/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ WEEK03/Assignment/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -15,43 +15,38 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   //--------------------------------------------------------------------------------
   def gen(level: Int): Gen[H] = for {
     i <- arbitrary[A]
-    //i <- arbitrary[A] if (i > Int.MinValue)
-    //i <- arbitrary[A] if (0 && i < Int.MaxValue) // Causes Gave up after 3 successful property evaluations. 98 evaluations were discarded.
-    //i <- arbitrary[A] if (Int.MinValue < i && i < Int.MaxValue)
     h <- oneOf(const(this.empty), gen(level + 1))
   } yield insert(i, h)
 
   lazy val genHeap: Gen[H] = gen(0)
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-  
   property("gen1") = forAll(genHeap) { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
 
-  property("On Empty, add then delete it is empty") = {
+  property("empty") = {
     isEmpty(deleteMin(insert(1, empty)))
   }
-  
-    property("Add two on Empty") = {
+
+  property("min of two") = {
     findMin(insert(2, insert(1, empty))) == 1
   }
 
-  property("Delete min then no more that min") = forAll(genHeap) { (h: H) =>
+  property("findMin(h) <= findMin(deleteMin(h))") = forAll(genHeap) { (h: H) =>
     if (isEmpty(h)) false
     else {
-      val rest = deleteMin(h)
-      if (isEmpty(rest)) true
-      else findMin(rest) >= findMin(h)
+      if (isEmpty(deleteMin(h))) true
+      else findMin(deleteMin(h)) >= findMin(h)
     }
   }
 
-  property("Min of melding is the same with that of being melded") = forAll(genHeap, genHeap) {
+  property("meld") = forAll(genHeap, genHeap) {
     (f: H, s: H) => findMin(meld(f, s)) == Math.min(findMin(f), findMin(s))
   }
 
-  property("First min is the minist of all") = forAll(genHeap) {
+  property("First min is the minimam of all") = forAll(genHeap) {
     (h: H) =>
       if (isEmpty(h)) false
       else {
@@ -66,7 +61,7 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
       else assureMin(theMin, deleteMin(rest))
     }
   }
-
+  /*
   property("sorted") = forAll(genHeap) {
     (h: H) =>
       if (isEmpty(h)) false
@@ -80,15 +75,15 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
       else checkSorted(cur, deleteMin(h))
     }
   }
-  
-  property("add and delete all") = {
+	*/
+  property("sorted") = {
     val h = ((0 until 1000).toList).foldLeft(empty)((heap, i) => insert(i, heap))
     checkAll(0, h)
   }
   def checkAll(min: Int, heap: H): Boolean = {
-    if(isEmpty(heap)) true
+    if (isEmpty(heap)) true
     else {
-      if(min != findMin(heap)) false
+      if (min != findMin(heap)) false
       else checkAll(min + 1, deleteMin(heap))
     }
   }

3f742901e0f44976c4bfe3b67db663ecaca05761
oonisim <monishi@wynyardgroup.com>
2016-09-05 12:13:46 +1000

    Calculator submit

--- WEEK04/Assignment/calculator/src/main/scala/calculator/Calculator.scala
+++ WEEK04/Assignment/calculator/src/main/scala/calculator/Calculator.scala
@@ -13,32 +13,24 @@ object Calculator {
     for ((name, exp) <- namedExpressions) yield (name -> Signal(eval(exp(), namedExpressions)))
   }
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    for ((name, exp) <- references) println("name %s expr %s".format(name, exp()))
-    expr match {
-      case Literal(v)  => v
-      case Plus(l, r)  => eval(l, references) + eval(r, references)
-      case Minus(l, r) => eval(l, references) - eval(r, references)
-      case Times(l, r) => eval(l, references) * eval(r, references)
-      case Divide(l, r) => {
-        if (eval(r, references) == 0) {
-          println("exp %s dividing by zero".format(expr))
-          return (Double.NaN)
-        } else eval(l, references) / eval(r, references)
-      }
-      case Ref(name) => {
-        if (!references.contains(name)) {
-          println("cyclic or non existing reference to %s".format(name))
-          return (Double.NaN)
-        } else {
-          //--------------------------------------------------------------------------------
-          // When variable 'X' refers to another variable, then remove 'X' from the references,
-          // so that additional reference to 'X' down in the reference recursion will be caught
-          // as a reference to non-existing variable.
-          //--------------------------------------------------------------------------------
-          eval(references(name)(), (references - name))
-        }
-      }
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = expr match {
+    case Literal(v)  => v
+    case Plus(l, r)  => eval(l, references) + eval(r, references)
+    case Minus(l, r) => eval(l, references) - eval(r, references)
+    case Times(l, r) => eval(l, references) * eval(r, references)
+/*    case Divide(l, r) => {
+      if (eval(r, references) == 0) Double.NaN // Divide by zero
+      else eval(l, references) / eval(r, references)
+    }*/
+    case Divide(l, r) => eval(l, references) / eval(r, references)
+    case Ref(name) => { 
+      if (!references.contains(name)) Double.NaN //println("cyclic or non existing reference to %s".format(name))
+      //--------------------------------------------------------------------------------
+      // When variable 'X' refers to another variable, then remove 'X' from the references,
+      // so that additional reference to 'X' down in the reference recursion will be caught
+      // as a reference to non-existing variable.
+      //--------------------------------------------------------------------------------
+      else eval(references(name)(), (references - name))
     }
   }
 
